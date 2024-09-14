cdc70a8ca7c23d45e509f1365beb6bd1474a13a2
Eric Graham <eric.graham@gmail.com>
2024-09-14 17:20:38 -0500

    example power functions from lectures

---
+++ [04]/power_notes.R
@@ -0,0 +1,4 @@
+power.t.test(n = 25, delta = 1, sd = 4, type = "one.sample", alternative = "two.sided", sig.level = .05)
+
+
+power.t.test(n = 65, delta = 1.6, sd = 4, type = "two.sample", alternative = "one.sided", sig.level = .05)
